1558220e5f124dfd202014fe618c1aeae27704ba
subrata <subrata@appsmith.com>
2025-03-18 10:26:18 +0000

    System generated commit, initial commit

---
+++ modules/JSModule1/jsobjects/JSModule1/JSModule1.js
@@ -0,0 +1,14 @@
+export default {
+	myVar1: [],
+	myVar2: {},
+	myFun1 () {
+		//	write code here
+		//	this.myVar1 = [1,2,3]
+		return GetUsers.run()
+	},
+	async myFun2 () {
+		//	use async-await or promises
+		//	await storeValue('varName', 'hello world')
+		return 33;
+	}
+}
